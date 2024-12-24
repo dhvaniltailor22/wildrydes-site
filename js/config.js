e6671aa0c698f8a82c124b1ddd53a0b21f4c04b3
dhvaniltailor22 <84226108+dhvaniltailor22@users.noreply.github.com>
2024-12-24 12:48:54 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
-window._config = {
+stwindow._config = {
     cognito: {
         userPoolId: 'us-east-1_K7O6I8Oox', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5k53ot7ise82tsl36r6h6dclkr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
