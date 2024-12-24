d0a878301d06f29df5b20437d57ea997e3a08131
dhvaniltailor22 <84226108+dhvaniltailor22@users.noreply.github.com>
2024-12-24 12:47:58 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_K7O6I8Oox', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5k53ot7ise82tsl36r6h6dclkr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
